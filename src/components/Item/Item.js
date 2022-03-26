1487b072921bcbb56d2048217e3dc8d6dbbf3b4a
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-03-26 20:03:25 +0600

    Complete Item component.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,16 +1,23 @@
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Item.css";
 
 const Item = ({ product }) => {
-  const { name, price, img } = product;
+  const { name, price, brand, img } = product;
   console.log(name);
   return (
     <div className="item">
       <img src={img} alt={name} />
       <h2>{name}</h2>
-      <h3>Price: ${price}</h3>
-      <button className="btn-addtocart">
+      <p className="brand">Brand: {brand}</p>
+      <p className="price">Price: ${price}</p>
+      <button className="btn btn-addtocart">
         <p>ADD TO CART</p>
+        <FontAwesomeIcon
+          className="icon"
+          icon={faCartShopping}
+        ></FontAwesomeIcon>
       </button>
     </div>
   );
