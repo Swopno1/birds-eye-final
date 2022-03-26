42ed0a07f9a05722d8824abf537331d6ad6875b1
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-03-26 20:33:52 +0600

    Cart completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,27 @@
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import "./Cart.css";
 
 const Cart = () => {
   return (
-    <div>
+    <aside className="cart">
       <h3>Selected Drones</h3>
-      <button className="btn-chose">
+      {/* This will be the generate dynamically */}
+      <div className="selected-items">
+        <img src="https://i.ibb.co/t4qHTMS/drone-1.jpg" alt="" />
+        <p>Product Name</p>
+        <button className="btn btn-delete">
+          <FontAwesomeIcon className="icon" icon={faTrashCan}></FontAwesomeIcon>
+        </button>
+      </div>
+      <button className="btn btn-choose">
         <p>CHOOSE 1 FOR ME</p>
       </button>
-      <button className="btn-reset">
+      <button className="btn btn-reset">
         <p>CHOOSE AGAIN</p>
       </button>
-    </div>
+    </aside>
   );
 };
 
